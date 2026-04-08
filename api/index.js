55338443ce3953c3e1107a617c59d6d557c606ba
Deepansh Sagar <sagardeepansh96@gmail.com>
2026-04-08 11:19:09 +0530

    added api folder for vercel

--- api/index.js
+++ api/index.js
@@ -13,15 +13,19 @@ app.use(express.json());
 
 app.use("/api", routes);
 
+app.get('/', (req, res) => {
+    res.send('Hello from Express on Vercel!');
+});
+
 // mongoose
 //   .connect('mongodb://127.0.0.1:27017/vuno')
 //   .then(() => console.log("MongoDB Connected"))
 //   .catch((err) => console.error(err));
 mongoose
-  .connect('mongodb+srv://admin:Q5ijo6823VV99FPc@cluster0.cs8pize.mongodb.net/vuno')
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error(err));
+    .connect('mongodb+srv://admin:Q5ijo6823VV99FPc@cluster0.cs8pize.mongodb.net/vuno')
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.error(err));
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+    console.log(`Server running on port ${process.env.PORT}`);
 });
